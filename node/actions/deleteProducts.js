1ae54a0eef45fe9612b5f5c82e3ea6de7f661aec
PikselGraphics <buk-kamil@o2.pl>
2022-02-10 09:40:49 +0100

    bug fixes

--- node/actions/deleteProducts.js
+++ node/actions/deleteProducts.js
@@ -1,8 +1,8 @@
-const User = require('../models/product')
+const Product = require('../models/product')
 
 const deleteProduct = async (req, res) => {
     try{
-        const producttodelete = await User.findByIdAndRemove(req.params.id);
+        const productDelete = await Product.findByIdAndRemove(req.params.id);
         res.status(200);
     } catch(err){
         console.log(err)
