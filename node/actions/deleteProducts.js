3c8f988592dda8e307b4e46cc6715a3e167c9bfc
PikselGraphics <buk-kamil@o2.pl>
2022-03-09 23:40:14 +0100

    plz work i want sleep

--- node/actions/deleteProducts.js
+++ node/actions/deleteProducts.js
@@ -11,19 +11,3 @@ const deleteProduct = async (req, res) => {
 }
 
 module.exports = {deleteProduct}
-=======
-const Product = require('../models/product')
-
-const deleteProduct = async (req, res) => {
-    try{
-        const productDelete = await Product.findByIdAndRemove(req.params.id);
-        res.status(200);
-    } catch(err){
-        console.log(err)
-        res.send({err})
-    }
-}
-
-
-
-module.exports = {deleteProduct}
