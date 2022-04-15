fe74da40640473b9cddcd0d546370b3e59bd478e
JakubKotulski <jakub.kotulski@interia.com>
2022-04-15 15:10:56 +0200

    fast fixing mates script problems on master

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("passport-local").Strategy;
 const express = require("express");
-const { loginAction } = require("./node/actions/loginAction");
+
+// -->
+
+// const { loginAction } = require("./node/actions/loginAction");
 const { addProduct } = require("./node/actions/addProduct.js"); 
 const { getProducts } = require("./node/actions/getProducts");
 const { markSoldProduct } = require("./node/actions/markSoldProduct");
 const { updateProduct } = require("./node/actions/updateProduct");
-const {deleteProduct} = require('./node/actions/deleteProducts')
+const {deleteProduct} = require('./node/actions/deleteProducts');
 
-const {registerAction} = require('./node/actions/register')
-const {userdelateAction} = require('./node/actions/userDelate')
-const {UpdateUser} = require('./node/actions/updateUser')
+const {registerAction} = require('./node/actions/register');
+const {userdelateAction} = require('./node/actions/userDelate');
+const {UpdateUser} = require('./node/actions/updateUser');
 
 mongoose.connect("mongodb+srv://praktyki:praktyki2021@development.wtktz.mongodb.net/school-project-backend", {
   useNewUrlParser: true,
@@ -40,7 +43,7 @@ app.delete("/product/:id/delete", deleteProduct);
 
 app.post('/user', registerAction);
 app.delete('/user/:id/delete', userdelateAction)
-app.put("/product/:id", UpdateUser);
+app.put("/user/:id", UpdateUser);
 
 app.listen(4000, () => {
     console.log("Server has started");
--- node/actions/deleteProducts.js
+++ node/actions/deleteProducts.js
@@ -2,28 +2,12 @@ const Product = require("../models/product")
 
 const deleteProduct = async (req, res) => {
     try{
-        const producttodelete = await Product.findByIdAndRemove(req.params.id);
+        await Product.findByIdAndRemove(req.params.id);
         res.status(200);
     }
     catch(err){
         console.log("err ", err);
     }
 }
-
-module.exports = {deleteProduct}
-=======
-const Product = require('../models/product')
-
-const deleteProduct = async (req, res) => {
-    try{
-        const productDelete = await Product.findByIdAndRemove(req.params.id);
-        res.status(200);
-    } catch(err){
-        console.log(err)
-        res.send({err})
-    }
-}
-
-
 
 module.exports = {deleteProduct}
